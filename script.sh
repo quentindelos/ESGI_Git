6d8226e00db493214689024deed84690643136b2
Quentin Delos <oxyloon.quenos@gmail.com>
2025-04-18 14:49:13 +0200

    first commit

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+REPERTOIRE_CIBLE="/home/quentin/Téléchargements"
+DOSSIER_ARCHIVE="$REPERTOIRE_CIBLE/archive"
+mkdir -p "$DOSSIER_ARCHIVE"
+find "$REPERTOIRE_CIBLE" -maxdepth 1 -type f -mtime +7 -exec mv {} "$DOSSIER_ARCHIVE" \;
