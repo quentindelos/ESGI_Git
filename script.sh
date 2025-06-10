1316dbe3d02d371204b9b10348d4dec42ad94756
Quentin Delos <oxyloon.quenos@gmail.com>
2025-06-10 08:32:56 +0200

    Add Shellcheck

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 REPERTOIRE_CIBLE="/home/quentin/Téléchargements"
 DOSSIER_ARCHIVE="$REPERTOIRE_CIBLE/archive"
 mkdir -p "$DOSSIER_ARCHIVE"
-find "$REPERTOIRE_CIBLE" -maxdepth 1 -type f -mtime +7 -exec mv {} "$DOSSIER_ARCHIVE" \;
+fqqqd "$REPERTOIRE_CIBLE" -maxdepth 1 -type f -mtime +7 -exec mv {} "$DOSSIER_ARCHIVE" \;
